c739015fa06185eafce4e7b0ee82dffdffd22cff
Karl Magnusson <karl.k.magnusson@gmail.com>
2017-01-10 20:14:33 +0100

    Better performance testing / reporting

--- src/karmag/jsapi/core.clj
+++ src/karmag/jsapi/core.clj
@@ -7,65 +7,6 @@
             [karmag.jsapi.resource :as r]
             [taoensso.carmine :as car :refer [wcar]]))
 
-;; before reflections removed
-
-;; Main running ...
-;;
-;; --------------------------------------------------
-;; karmag.jsapi.core$_main$fn__5835
-;;
-;; Completed 100000 tasks in 0.8 seconds [121212.1 / s]
-;;
-;; --------------------------------------------------
-;; karmag.jsapi.core$_main$write_resource_fast__5837
-;;
-;; Completed 100000 tasks in 3.5 seconds [28943.6 / s]
-;;
-;; --------------------------------------------------
-;; karmag.jsapi.core$_main$write_data__5841
-;;
-;; Completed 100000 tasks in 1.2 seconds [86132.6 / s]
-;;
-;; --------------------------------------------------
-;; karmag.jsapi.core$_main$parse_resource__5833
-;;
-;; Completed 100000 tasks in 20.7 seconds [4821.6 / s]
-;;
-;; --------------------------------------------------
-;; clojure.core$comp$fn__4727
-;;
-;; Completed 100000 tasks in 24.3 seconds [4117.4 / s]
-
-;; with reflection warnings fixed
-
-;; Main running ...
-;;
-;; --------------------------------------------------
-;; karmag.jsapi.core$_main$fn__5833
-;;
-;; Completed 100000 tasks in 0.8 seconds [120192.3 / s]
-;;
-;; --------------------------------------------------
-;; karmag.jsapi.core$_main$write_resource_fast__5835
-;;
-;; Completed 100000 tasks in 3.9 seconds [25413.0 / s]
-;;
-;; --------------------------------------------------
-;; karmag.jsapi.core$_main$write_data__5839
-;;
-;; Completed 100000 tasks in 1.2 seconds [82169.3 / s]
-;;
-;; --------------------------------------------------
-;; karmag.jsapi.core$_main$parse_resource__5831
-;;
-;; Completed 100000 tasks in 3.4 seconds [29420.4 / s]
-;;
-;; --------------------------------------------------
-;; clojure.core$comp$fn__4727
-;;
-;; Completed 100000 tasks in 6.1 seconds [16455.5 / s]
-
-
 (def definition
   (-> "test-definition.clj" io/resource d/read-definition))
 
@@ -98,9 +39,7 @@
 (defn -main []
   (println "Main running ...")
   (try
-    (let [amount (long 100000)
-
-          gen-id #(str (rand-int Integer/MAX_VALUE))
+    (let [gen-id #(str (rand-int Integer/MAX_VALUE))
 
           parse-resource (fn []
                            (let [res (:data (parse-json js-data))]
@@ -116,28 +55,37 @@
           write-data (fn [] (wcar {:host "localhost" :port 6379}
                                   (car/set (gen-id) "data goes here")))
 
-          fns [gen-resource
-               write-resource-fast
-               write-data
-               parse-resource
-               (comp write-resource parse-resource)]]
+          fns [["small write" write-data]
+               ["resource write" write-resource-fast]
+               ["resource parse" parse-resource]
+               ["parse + write" (comp write-resource parse-resource)]]]
 
-      (doseq [f fns]
-        (println)
-        (println "--------------------------------------------------")
-        (println (type f))
-        (let [start (System/currentTimeMillis)]
-          (->> (repeat amount f)
-               (pmap #(%))
-               dorun)
-          #_(dotimes [n amount]
-              (f))
-          (let [end (System/currentTimeMillis)
-                secs (/ (- end start) 1000)]
-            (println)
-            (println (format "Completed %d tasks in %.1f seconds [%.1f / s]"
-                             amount
-                             (float secs)
-                             (float (/ amount secs))))))))
+      (doseq [[f-name f] fns]
+        (doseq [thread-count [2 4 8 10 12 16 32]]
+          (let [counter (atom 0)
+                running (atom true)
+                threads (->> #(Thread. (fn [] (while @running
+                                                (f)
+                                                (swap! counter inc))))
+                             repeatedly
+                             (take thread-count)
+                             doall)]
+            (doseq [t threads] (.start ^Thread t))
+            (let [start-count @counter
+                  start-time (System/currentTimeMillis)]
+              (Thread/sleep 5000)
+              (let [end-count @counter
+                    end-time (System/currentTimeMillis)]
+                (reset! running false)
+                (doseq [t threads] (.join ^Thread t))
+                (let [processed (- end-count start-count)
+                      seconds (float (/ (- end-time start-time)
+                                        1000))]
+                  (println (format "%s -- %2d threads [%.1f / s] %d executions, %.1f sec"
+                                   f-name
+                                   thread-count
+                                   (/ processed seconds)
+                                   processed
+                                   seconds)))))))))
     (finally
       (shutdown-agents))))
